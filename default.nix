9f3ccd2e1895c2e1bdb347eaf32e7e5e65240c0f
Thales Macedo Garitezi <tmacedo@xerpa.com.br>
2020-01-20 16:48:06 -0300

    rascunho frp

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@ let nixpkgs = import ./nixpkgs.nix;
 in {
   prolog-coquetel = nixpkgs.callPackage ./prolog-coquetel { };
   polysemy-vdp = nixpkgs.callPackage ./polysemy-vdp { };
+  frp = nixpkgs.callPackage ./frp { };
 }
