80093984a6808a73b9707bf54572f6fc782520cd
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-01-21 12:37:09 -0300

    Gênese (#1)

---
+++ default.nix
@@ -0,0 +1,6 @@
+let nixpkgs = import ./nixpkgs.nix;
+in {
+  prolog-coquetel = nixpkgs.callPackage ./prolog-coquetel { };
+  polysemy-vdp = nixpkgs.callPackage ./polysemy-vdp { };
+  frp = nixpkgs.callPackage ./frp { };
+}
