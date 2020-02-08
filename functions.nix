18a338e70b29a8c9b20a77885ea87aa18ffc3f4a
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-02-08 09:36:11 -0300

    hhhhhhhhhhhhhhhhhhhhhhhhhhh

--- functions.nix
+++ functions.nix
@@ -2,5 +2,7 @@ with (import ./nixpkgs.nix);
 buildGoModule {
   name = "lambda-functions";
   src = ./netlify/fns;
+  buildInputs = [ glibc.static ];
   modSha256 = "1f4axlzjsikma2ccqihyjkz5b8f9gyi643am855imy9mi17mggh4";
+  LDFLAGS = "-s -w -linkmode external -extldflags -static";
 }
