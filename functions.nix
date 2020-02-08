8aae456ec0f2a48a8280edf21997c439086ab7c6
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-02-08 10:14:49 -0300

    iiiiiiiiiiiiiiiiiiiiiiiiiiii

--- functions.nix
+++ functions.nix
@@ -1,8 +1,10 @@
-with (import ./nixpkgs.nix);
+with (import ./nixpkgs.nix).pkgsMusl;
 buildGoModule {
   name = "lambda-functions";
   src = ./netlify/fns;
   buildInputs = [ glibc.static ];
   modSha256 = "1f4axlzjsikma2ccqihyjkz5b8f9gyi643am855imy9mi17mggh4";
+  # CFLAGS="-I${pkgs.glibc.dev}/include";
+  # LDFLAGS="-L${pkgs.glibc}/lib";
   LDFLAGS = "-s -w -linkmode external -extldflags -static";
 }
