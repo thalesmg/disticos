200d92220473785c00408f78f60d7e8f8d01cb23
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-02-09 13:47:22 -0300

    complexo

---
+++ obj.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+cd /mnt
+
+ibmcloud login --apikey "$IBMC_API_KEY" -r us-south
+
+case "$1" in
+  put)
+    ibmcloud cos put-object \
+      --bucket baldinho-cos-standard-ozm \
+      --key "$TRAVIS_BUILD_ID/$2" \
+      --body "$2"
+    ;;
+
+  get)
+    ibmcloud cos get-object \
+      --bucket baldinho-cos-standard-ozm \
+      --key "$TRAVIS_BUILD_ID/$2" \
+      --body "$2"
+    ;;
+esac
