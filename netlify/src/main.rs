d964f6b7eb80e3891d8eca8d4e2a7a162649baa1
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-02-09 10:20:10 -0300

    teste 2

--- netlify/src/main.rs
+++ netlify/src/main.rs
@@ -6,6 +6,9 @@ use serde_derive::{Deserialize, Serialize};
 use simple_logger;
 
 #[derive(Deserialize)]
+struct Nada {}
+
+#[derive(Deserialize)]
 struct CustomEvent {
     #[serde(rename = "firstName")]
     first_name: String,
@@ -23,13 +26,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        error!("Empty first name in request {}", c.aws_request_id);
-        Err(HandlerError::from("Empty first name!"))
-    } else {
-        Ok(CustomOutput {
-            message: format!("Hello, {}!", e.first_name),
-        })
-    }
+fn my_handler(e: Nada, c: Context) -> Result<CustomOutput, HandlerError> {
+    // if e.first_name == "" {
+    //     error!("Empty first name in request {}", c.aws_request_id);
+    //     Err(HandlerError::from("Empty first name!"))
+    // } else {
+    //     Ok(CustomOutput {
+    //         message: format!("Hello, {}!", e.first_name),
+    //     })
+    // }
+    Ok(CustomOutput {
+        message: format!("Oi, mundo!"),
+    })
 }
