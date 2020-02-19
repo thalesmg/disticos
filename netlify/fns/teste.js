cc3fd393f075c1047ad15ebe7b9395b86d377124
Thales Macedo Garitezi <thalesmg@gmail.com>
2020-02-19 20:25:35 -0300

    mais teste

--- netlify/fns/teste.js
+++ netlify/fns/teste.js
@@ -1,6 +1,31 @@
+// exports.handler = function(event, context, callback) {
+//     callback(null, {
+//     statusCode: 200,
+//     body: "Cache chato"
+//     });
+// }
+
+const requests = require("request");
+
 exports.handler = function(event, context, callback) {
-    callback(null, {
-    statusCode: 200,
-    body: "Cache chato"
+    requests({
+        url: "https://httpbin.org/post",
+        method: "POST",
+        jar: true,
+        qs: {
+            "token": "10"
+        }
+    }, (error, res, body) => {
+        if (error) {
+            callback(null, {
+                statusCode: 500,
+                body: "Deu ruim!"
+            });
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: body
+            })
+        }
     });
-}
+};
